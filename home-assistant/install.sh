5e706bfc4c29c65171c6070119aa89d7ca5df6f9
Martin Blitz <mcblitz@verizon.net>
2019-11-04 21:25:45 -0500

    Remote old home assistant

--- home-assistant/install.sh
+++
@@ -1,4 +0,0 @@
-kubectl apply -f home-assistant-pv-volume.yaml
-kubectl apply -f home-assistant-pv-claim.yaml
-helm install --name home-assistant stable/home-assistant -f values.yaml
-
--- home-assistant/uninstall.sh
+++
@@ -1,3 +0,0 @@
-helm delete --purge home-assistant
-kubectl delete -f home-assistant-pv-claim.yaml
-kubectl delete -f home-assistant-pv-volume.yaml
