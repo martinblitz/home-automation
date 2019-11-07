a35eb38712b3a5590788102e3670b233d876fcdc
Martin Blitz <mcblitz@verizon.net>
2019-11-06 20:50:52 -0500

    Added metallb

---
+++ metallb/install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
+kubectl apply -f metallb-configmap.yaml
---
+++ metallb/uninstall.sh
@@ -0,0 +1,2 @@
+kubectl delete -f metallb-configmap.yaml
+kubectl delete -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
