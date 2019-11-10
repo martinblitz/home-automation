fde03f6af98df546ef8fb1bac4860848fb455f66
Martin Blitz <mcblitz@verizon.net>
2019-11-10 12:51:35 -0500

    Use DNS for nfs-server

--- home-assistant/install.sh
+++ home-assistant/install.sh
@@ -2,5 +2,7 @@
 
 kubectl apply -f home-assistant-config-pv-volume.yaml
 kubectl apply -f home-assistant-config-pv-claim.yaml
+kubectl apply -f home-assistant-ssl-pv-volume.yaml
+kubectl apply -f home-assistant-ssl-pv-claim.yaml
 kubectl apply -f home-assistant-deployment.yaml
 kubectl apply -f home-assistant-service.yaml
--- home-assistant/uninstall.sh
+++ home-assistant/uninstall.sh
@@ -2,5 +2,7 @@
 
 kubectl delete -f home-assistant-service.yaml
 kubectl delete -f home-assistant-deployment.yaml
+kubectl delete -f home-assistant-ssl-pv-claim.yaml
+kubectl delete -f home-assistant-ssl-pv-volume.yaml
 kubectl delete -f home-assistant-config-pv-claim.yaml
 kubectl delete -f home-assistant-config-pv-volume.yaml
