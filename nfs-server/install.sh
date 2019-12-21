414a853083e7dcf97a93b497c3f559dfaf4d37fa
Martin Blitz <mcblitz@verizon.net>
2019-12-20 20:08:15 -0500

    added backup nfs export

--- nfs-server/install.sh
+++ nfs-server/install.sh
@@ -2,6 +2,8 @@
 
 kubectl apply -f nfs-server-alpine-pv-volume.yaml
 kubectl apply -f nfs-server-alpine-pv-claim.yaml
+kubectl apply -f nfs-server-alpine-backup-pv-volume.yaml
+kubectl apply -f nfs-server-alpine-backup-pv-claim.yaml
 kubectl apply -f nfs-server-alpine-deployment.yaml
 kubectl apply -f nfs-server-alpine-service.yaml
 
--- nfs-server/uninstall.sh
+++ nfs-server/uninstall.sh
@@ -2,6 +2,8 @@
 
 kubectl delete -f nfs-server-alpine-service.yaml
 kubectl delete -f nfs-server-alpine-deployment.yaml
+kubectl delete -f nfs-server-alpine-backup-pv-claim.yaml
+kubectl delete -f nfs-server-alpine-backup-pv-volume.yaml
 kubectl delete -f nfs-server-alpine-pv-claim.yaml
 kubectl delete -f nfs-server-alpine-pv-volume.yaml
 
