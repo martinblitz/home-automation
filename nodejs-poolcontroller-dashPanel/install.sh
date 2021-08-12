82c326855b0c8555ee2285d81db63ed08d44f2ef
Ubuntu <ubuntu@k8s-node-1.blitzhome.local>
2021-08-12 19:18:52 +0000

    Add nodejs-poolcontroller-dashPanel

---
+++ nodejs-poolcontroller-dashPanel/install.sh
@@ -0,0 +1,5 @@
+kubectl apply -f nodejs-poolcontroller-dashPanel-pv-volume.yaml
+kubectl apply -f nodejs-poolcontroller-dashPanel-pv-claim.yaml
+kubectl apply -f nodejs-poolcontroller-dashPanel-deployment.yaml
+kubectl apply -f nodejs-poolcontroller-dashPanel-service.yaml
+
---
+++ nodejs-poolcontroller-dashPanel/uninstall.sh
@@ -0,0 +1,4 @@
+kubectl delete -f nodejs-poolcontroller-dashPanel-service.yaml
+kubectl delete -f nodejs-poolcontroller-dashPanel-deployment.yaml
+kubectl delete -f nodejs-poolcontroller-dashPanel-pv-claim.yaml
+kubectl delete -f nodejs-poolcontroller-dashPanel-pv-volume.yaml
